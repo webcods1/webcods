4b09b80618ff7e6e2790d8e3e05f059a6efccfc9
webcods1 <webcods1@gmail.com>
2026-01-28 12:58:28 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -958,7 +958,7 @@ function App() {
                                                 className="
                                                 w-full px-2.5 sm:px-3 md:px-4 py-2 sm:py-2 md:py-3
                                                 bg-white/10 border border-white/20 rounded-lg md:rounded-xl
-                                                text-xs sm:text-sm md:text-base text-white placeholder-gray-400
+                                                text-base text-white placeholder-gray-400
                                                 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent
                                                 backdrop-blur-sm transition-all duration-300
                                                 "
@@ -971,7 +971,7 @@ function App() {
                                                 className="
                                                 w-full px-2.5 sm:px-3 md:px-4 py-2 sm:py-2 md:py-3
                                                 bg-white/10 border border-white/20 rounded-lg md:rounded-xl
-                                                text-xs sm:text-sm md:text-base text-white placeholder-gray-400
+                                                text-base text-white placeholder-gray-400
                                                 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent
                                                 backdrop-blur-sm transition-all duration-300
                                                 "
@@ -986,7 +986,7 @@ function App() {
                                                 className="
                                                 w-full px-2.5 sm:px-3 md:px-4 py-2 sm:py-2 md:py-3
                                                 bg-white/10 border border-white/20 rounded-lg md:rounded-xl
-                                                text-xs sm:text-sm md:text-base text-white placeholder-gray-400
+                                                text-base text-white placeholder-gray-400
                                                 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent
                                                 backdrop-blur-sm transition-all duration-300
                                                 "
